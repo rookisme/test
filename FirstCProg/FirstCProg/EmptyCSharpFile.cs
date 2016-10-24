a9d3c0d53c6754c84ce40cff9a6a29fdf0f37308
rookisme <github.com@rooksa.com>
2016-10-24 23:08:28 +0300

    Add files via upload

---
+++ FirstCProg/FirstCProg/EmptyCSharpFile.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+namespace letstry
+{
+	class divbyzer
+	{
+		public static void Main(string[] args)
+		{
+			int numbr1, numbr2;
+			float total;
+			Console.Write("Enter the first number: ");
+			numbr1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("\nEnter the 2nd number: ");
+			numbr2 = Convert.ToInt32(Console.ReadLine());
+			try
+			{
+				numbr2 -= numbr2;
+				total = numbr1 / numbr2;
+			}
+			catch (DivideByZeroException error1)
+			{
+				Console.WriteLine("While dividing {0} / {1} we reached an invalid conclusion");
+			}
+		}
+	}
+}
+*/
